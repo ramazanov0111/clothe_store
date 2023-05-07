3a29dbfe989d39ecbf0619dc93f1f32e40ca64f1
Ренат Рамазанов <renat@eljur.team>
2023-05-07 19:44:32 +0300

    refactor

--- resources/views/client/layouts/main.blade.php
+++ resources/views/client/layouts/main.blade.php
@@ -4,50 +4,36 @@
     <title>Store</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--===============================================================================================-->
-<link rel="icon" type="image/png" href="{{ asset('assets/images/icons/favicon.png') }}"/>
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/iconic/css/material-design-iconic-font.min.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/linearicons-v1.0.0/icon-font.min.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/animate/animate.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/css-hamburgers/hamburgers.min.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/animsition/css/animsition.min.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/select2/select2.min.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/daterangepicker/daterangepicker.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/slick/slick.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/MagnificPopup/magnific-popup.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.css') }}">
-    <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/util.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/main.css') }}">
-    <!--===============================================================================================-->
+
+    <base href="/">
+
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/icons/favicon.png') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('assets/fonts/iconic/css/material-design-iconic-font.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/linearicons-v1.0.0/icon-font.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/animate/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/animsition/css/animsition.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/select2/select2.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/slick/slick.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/MagnificPopup/magnific-popup.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/main.css') }}">
 
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 @yield('content')
 
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/animsition/js/animsition.min.js') }}"></script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/bootstrap/js/popper.js') }}"></script>
 <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/select2/select2.min.js') }}"></script>
 <script>
     // $(document).on('change', function () {
@@ -59,18 +45,14 @@
     })
     // });
 </script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/daterangepicker/moment.min.js') }}"></script>
 <script src="{{ asset('assets/vendor/daterangepicker/daterangepicker.js') }}"></script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/slick/slick.min.js') }}"></script>
 <script src="{{ asset('assets/js/slick-custom.js') }}"></script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/parallax100/parallax100.js') }}"></script>
 <script>
     $('.parallax100').parallax100();
 </script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/MagnificPopup/jquery.magnific-popup.min.js') }}"></script>
 <script>
     $('.gallery-lb').each(function () { // the containers for all your galleries
@@ -84,9 +66,7 @@
         });
     });
 </script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/isotope/isotope.pkgd.min.js') }}"></script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/sweetalert/sweetalert.min.js') }}"></script>
 <script>
     $('.js-addwish-b2').on('click', function (e) {
@@ -123,7 +103,6 @@
         });
     });
 </script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
 <script>
     $('.js-pscroll').each(function () {
@@ -140,7 +119,6 @@
         })
     });
 </script>
-<!--===============================================================================================-->
 <script src="{{ asset('assets/js/main.js') }}"></script>
 
 </html>
