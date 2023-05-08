7d584f6dfe75d10e3f5e56fae3bfbb2d17d4dac8
Ренат Рамазанов <renat@eljur.team>
2023-05-08 16:19:28 +0300

    refactor

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from "axios";
+import express from "express";
 
 require('./bootstrap');
 
@@ -10,3 +11,4 @@ const app = createApp(App)
 app.use(router)
 app.config.globalProperties.axios = axios
 app.mount('#app')
+app.use(express.static('js'));
