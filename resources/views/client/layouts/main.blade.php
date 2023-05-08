1c27b5e5d0d84d55a359652850a2a1502a4bd83f
Ренат Рамазанов <renat@eljur.team>
2023-05-08 18:01:29 +0300

    refactor

--- resources/views/client/layouts/main.blade.php
+++ resources/views/client/layouts/main.blade.php
@@ -6,36 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="icon" type="image/png" href="{{ asset('assets/images/icons/favicon.png') }}"/>
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css"
-          href="{{ asset('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" type="text/css"
-          href="{{ asset('assets/fonts/iconic/css/material-design-iconic-font.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/linearicons-v1.0.0/icon-font.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/animate/animate.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/css-hamburgers/hamburgers.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/animsition/css/animsition.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/select2/select2.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/daterangepicker/daterangepicker.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/slick/slick.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/MagnificPopup/magnific-popup.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/util.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}"/>
+    <link rel="stylesheet"
+          href="{{ asset('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}"/>
+    <link rel="stylesheet"
+          href="{{ asset('assets/fonts/iconic/css/material-design-iconic-font.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/fonts/linearicons-v1.0.0/icon-font.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/animate/animate.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css-hamburgers/hamburgers.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/animsition/css/animsition.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/select2/select2.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/daterangepicker/daterangepicker.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/slick/slick.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/MagnificPopup/magnific-popup.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/util.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}"/>
 
-    <script src="{{ asset('js/manifest.js') }}" defer></script>
-    <script src="{{ asset('js/vendor.js') }}" defer></script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script type="text/javascript" src="{{ asset('js/manifest.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/vendor.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 @yield('content')
 
-<script src="{{ asset('assets/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/animsition/js/animsition.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/bootstrap/js/popper.js') }}"></script>
-<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/select2/select2.min.js') }}"></script>
-<script>
+<script type="text/javascript" src="{{ asset('assets/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/animsition/js/animsition.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/bootstrap/js/popper.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/select2/select2.min.js') }}"></script>
+<script type="text/javascript">
     // $(document).on('change', function () {
     $(".js-select2").each(function () {
         $(this).select2({
@@ -45,16 +45,16 @@
     })
     // });
 </script>
-<script src="{{ asset('assets/vendor/daterangepicker/moment.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/daterangepicker/daterangepicker.js') }}"></script>
-<script src="{{ asset('assets/vendor/slick/slick.min.js') }}"></script>
-<script src="{{ asset('assets/js/slick-custom.js') }}"></script>
-<script src="{{ asset('assets/vendor/parallax100/parallax100.js') }}"></script>
-<script>
+<script type="text/javascript" src="{{ asset('assets/vendor/daterangepicker/moment.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/daterangepicker/daterangepicker.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/slick/slick.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/slick-custom.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/parallax100/parallax100.js') }}"></script>
+<script type="text/javascript">
     $('.parallax100').parallax100();
 </script>
-<script src="{{ asset('assets/vendor/MagnificPopup/jquery.magnific-popup.min.js') }}"></script>
-<script>
+<script type="text/javascript" src="{{ asset('assets/vendor/MagnificPopup/jquery.magnific-popup.min.js') }}"></script>
+<script type="text/javascript">
     $('.gallery-lb').each(function () { // the containers for all your galleries
         $(this).magnificPopup({
             delegate: 'a', // the selector for gallery item
@@ -66,9 +66,9 @@
         });
     });
 </script>
-<script src="{{ asset('assets/vendor/isotope/isotope.pkgd.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/sweetalert/sweetalert.min.js') }}"></script>
-<script>
+<script type="text/javascript" src="{{ asset('assets/vendor/isotope/isotope.pkgd.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/sweetalert/sweetalert.min.js') }}"></script>
+<script type="text/javascript">
     $('.js-addwish-b2').on('click', function (e) {
         e.preventDefault();
     });
@@ -103,8 +103,8 @@
         });
     });
 </script>
-<script src="{{ asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
-<script>
+<script type="text/javascript" src="{{ asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<script type="text/javascript">
     $('.js-pscroll').each(function () {
         $(this).css('position', 'relative');
         $(this).css('overflow', 'hidden');
@@ -119,6 +119,6 @@
         })
     });
 </script>
-<script src="{{ asset('assets/js/main.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/main.js') }}"></script>
 
 </html>
